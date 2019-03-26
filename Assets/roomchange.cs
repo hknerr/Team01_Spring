433be3f2a64d19ea9b701ede362ce8f8579c11cd
hknerr <48099005+hknerr@users.noreply.github.com>
2019-03-26 13:07:42 -0400

    added room change script

---
+++ Assets/roomchange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class roomchange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("dungeon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
