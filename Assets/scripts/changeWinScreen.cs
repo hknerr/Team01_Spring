016a241749613dbf2a080eb71ddfe0918cdfd511
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-04-02 12:20:30 -0400

    Added Scripts
    added scripts to bedroom,library and ending

---
+++ Assets/scripts/changeWinScreen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+// Brandon Salter
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changediner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("WinScreen");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
