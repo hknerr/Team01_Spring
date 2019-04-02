386a95ab74fddb24cc9da6f8ecfbbceed84f4849
themightykrang <48098967+themightykrang@users.noreply.github.com>
2019-04-02 10:34:26 -0400

    courtyard and storage now change rooms
    I added the scripts so the courtyard and storage room scenes now change rooms.

---
+++ Assets/scripts/changediner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+// John Hangen
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changediner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("diner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
