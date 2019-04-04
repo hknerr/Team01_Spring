6917d160839f829b5f996848ea445c5c59126fe6
sweettooth115 <48257835+sweettooth115@users.noreply.github.com>
2019-04-04 16:13:27 -0400

    fixed doors
    for my rooms

---
+++ Assets/scripts/changeCourtyard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeCourtyard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("courtyard");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
