9e51886e05dbbe94d54b1e1e77a7ae0b3ebb0878
CIS Student <TC210-Student@webdevlccc.com>
2019-04-18 11:05:55 -0400

    test
    menu test ran out of time before class.  will continue at home.

---
+++ Assets/scripts/Play.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+
+    public void GoToStory(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ReturnToMenu(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
