5b7592627eb7ba7d8d3c582cf5f4f28849bf7788
CIS Student <TC210-Student@webdevlccc.com>
2019-04-18 10:40:00 -0400

    main menu script
    added the main menu script

---
+++ Assets/scripts/Main Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//John Hangen
+public class Start : MonoBehaviour
+{
+
+    public void LoadByIndex(int sceneIndex)
+
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+}
