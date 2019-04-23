a00130ee6047bbc95b7e5057a0aa40c977693983
themightykrang <48098967+themightykrang@users.noreply.github.com>
2019-04-23 12:56:37 -0400

    pause menu
    started the pause menu

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+    //John Hangen
+{
+    public GameObject Pausemenu, PauseButton;
+    //makes everything in the game pause
+    public void Pause ()
+    {
+        Pausemenu.SetActive (true);
+        PauseButton.SetActive (false);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        Pausemenu.SetActive (false);
+        PauseButton.SetActive (true);
+        Time.timeScale = 1;
+    }
+}
