9d0eaabf5ecd304ca793de2d954db70b7f32d136
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-04-23 12:52:49 -0400

    Jiggle billy sound effects

---
+++ Assets/scripts/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Brandon Salter
+public class SoundPlayer : MonoBehaviour
+{
+    // Creative idea, on mouse click plays an audio source
+    private void OnMouseDown()
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+
+}
