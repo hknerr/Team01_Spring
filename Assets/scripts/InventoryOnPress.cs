9ead98d0ce831fb73a81e4c82eb7b46975975dfa
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <18655689+pompossum@users.noreply.github.com>
2019-04-23 12:29:45 -0400

    gui examine text thingie
    hhhhhhh

---
+++ Assets/scripts/InventoryOnPress.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryOnPress : MonoBehaviour
+{
+    public bool drawingGUI = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            drawingGUI = true;
+    }
+
+    void OnGUI()
+    {
+        if (drawingGUI)
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+                drawingGUI = false;
+        }
+    }
+}
