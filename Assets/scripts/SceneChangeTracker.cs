fece6e5f5fdad6c325e3a7cda3cc62523f8724a8
hknerr <48099005+hknerr@users.noreply.github.com>
2019-04-25 11:53:21 -0400

    made inventory persist

---
+++ Assets/scripts/SceneChangeTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChangeTracker : MonoBehaviour
+{
+    public GameObject theGameController;
+    public GameControlScript theControllerScript;
+    public GameObject invGameObj;
+    public Text newItem;
+
+    void OnEnable()
+    {
+        theGameController = GameObject.Find("GameController");
+        theControllerScript = theGameController.GetComponent<GameControlScript>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            invGameObj = GameObject.Find("Canvas/Vertical Panel/Inventory" + (i+1));
+            newItem = invGameObj.GetComponent<Text>();
+            newItem.text = theControllerScript.ItemsinInventory[i];
+
+
+            Debug.Log("Setting inv position" + i);
+        }
+
+    }
+}
