8f6643738ce1b998cecd5fb407ddbe267c37cbbd
hknerr <48099005+hknerr@users.noreply.github.com>
2019-04-25 12:41:12 -0400

    1

---
+++ Assets/scripts/toggleui.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleui : MonoBehaviour
+{
+    public bool enableUI = true;
+    void update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            enableUI = !enableUI;
+            this.enabled = enableUI;
+            Debug.Log("I pressed");
+        }
+    }
+}
