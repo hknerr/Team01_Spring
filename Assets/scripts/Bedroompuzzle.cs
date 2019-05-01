09aa5fe0223da640ad4cb55ab2bc0898ddf90154
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-05-01 12:00:01 -0400

    fixed rooms

---
+++ Assets/scripts/Bedroompuzzle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bedroompuzzle : MonoBehaviour
+{
+    public GameObject theGameController;
+    public GameControlScript theControllerScript;
+    public Text myText;
+    public Text newItem;
+    public GameObject myGameObject;
+    public GameObject invGameObj;
+
+
+    private void OnMouseDown()
+    {
+
+        theGameController = GameObject.Find("GameController");
+        theControllerScript = theGameController.GetComponent<GameControlScript>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            invGameObj = GameObject.Find("Canvas/Vertical Panel/Inventory" + (i + 1));
+            newItem = invGameObj.GetComponent<Text>();
+            if (newItem.text == "screw driver")
+            {
+                newItem.text = "Inventory";
+            }
+            else
+            {
+                Debug.Log("NO SCREW DRIVER");
+            }
+
+        }
+
+    }
+}
