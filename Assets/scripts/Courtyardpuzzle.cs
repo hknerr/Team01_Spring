836fe0e6533c3f9929d2c026461ed5dd64a6a9b9
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-05-02 12:29:20 -0400

    rip in pepporioni

---
+++ Assets/scripts/Courtyardpuzzle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Courtyardpuzzle : MonoBehaviour
+{
+    public GameObject theGameController;
+    public GameControlScript theControllerScript;
+    public Text myText;
+    public Text newItem;
+    public GameObject myGameObject;
+    public GameObject invGameObj;
+    private void OnMouseDown()
+    {
+        theGameController = GameObject.Find("GameController");
+        theControllerScript = theGameController.GetComponent<GameControlScript>();
+
+        for (int i = 0; i < 10; i++)
+        {
+            invGameObj = GameObject.Find("Canvas/Vertical Panel/Inventory" + (i + 1));
+            newItem = invGameObj.GetComponent<Text>();
+            if (newItem.text == "Statue 1" + "Statue 2" + "Statue 3")
+            {
+                newItem.text = "";
+            }
+
+            else
+            {
+                Debug.Log("NONE OF THE STATUES ARE COLLECTED");
+            }
+
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if (theControllerScript.ItemsinInventory[i] == "Statue 1" + "Statue 2" + "Statue 3")
+            {
+                theControllerScript.ItemsinInventory[i] = "";
+            }
+        }
+
+    }
+}
