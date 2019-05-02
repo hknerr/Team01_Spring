1843817e79ab7819b6cdf68aff646b9cad078c2f
themightykrang <48098967+themightykrang@users.noreply.github.com>
2019-05-02 11:50:46 -0400

    kk

--- Assets/scripts/Roomchange/changecellar.cs
+++ Assets/scripts/Roomchange/changecellar.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 //Christa
+// John Hangen
+//This code allows the player to leave the courtyard scene and enter the diner scene.
+//In the courtyard scene its attached to courtyard door object.
 public class changecellar : MonoBehaviour
 {
     // changes to cellar attached to cellar door
