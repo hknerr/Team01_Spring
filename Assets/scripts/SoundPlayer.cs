1ddca69baf33c178cb237749dbb7efc9a474eaf9
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-05-02 12:19:50 -0400

    Update SoundPlayer.cs

--- Assets/scripts/SoundPlayer.cs
+++ Assets/scripts/SoundPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SoundPlayer : MonoBehaviour
 {
     // Creative idea, on mouse click plays an audio source
+    //Plays a sound when clicking on an object; Attached to: Iowa, Bird; Fireplace and Jiggle billy.
     private void OnMouseDown()
     {
         this.GetComponent<AudioSource>().Play();
