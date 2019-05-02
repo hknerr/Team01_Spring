26caba6574e703832cd4d9335f5c065f8ee8d299
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-05-02 14:42:16 -0400

    easter egg room and library fix

---
+++ Assets/scripts/Roomchange/changelibrar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changelibrar : MonoBehaviour
+{
+    //brandon Salter
+    //easter egg room
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("library");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
