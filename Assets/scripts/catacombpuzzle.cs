a9805f10af8ae6fec8ab47f5fe38e3a55b1800b0
AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA <18655689+pompossum@users.noreply.github.com>
2019-05-02 12:50:10 -0400

    latest
    h

---
+++ Assets/scripts/catacombpuzzle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class catacombpuzzle : MonoBehaviour
+{
+    public GameObject theGameController;
+    public GameControlScript theControllerScript;
+    public Text myText;
+    public Text newItem;
+    public GameObject myGameObject;
+    public GameObject invGameObj;
+    public GameObject itemToDelete;
+    public GameObject theStatue;
+
+    void Start()
+    {
+        // Hide skull
+        GameObject.Find("skullhead").transform.localPosition = new Vector3(100, 100, 0);
+    }
+    private void OnMouseDown()
+    {
+        theGameController = GameObject.Find("GameController");
+        theControllerScript = theGameController.GetComponent<GameControlScript>();
+
+
+        for (int i = 0; i < 10; i++)
+        {
+            invGameObj = GameObject.Find("Canvas/Vertical Panel/Inventory" + (i + 1));
+            newItem = invGameObj.GetComponent<Text>();
+            if (newItem.text == "skull")
+            {
+                theControllerScript.deletePosition += 1;
+                newItem.text = "";
+                theControllerScript.ItemsToDelete[theControllerScript.deletePosition - 1] = "skull";
+
+                // Show skull
+                GameObject.Find("skullhead").transform.localPosition = new Vector3(0.16f, -2.11f, 0f);
+
+                itemToDelete = GameObject.Find("cellar door locked");
+                GameObject.Destroy(itemToDelete);
+            }
+
+            else
+            {
+                Debug.Log("hhh");
+            }
+
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            if (theControllerScript.ItemsinInventory[i] == "skull")
+            {
+                theControllerScript.ItemsinInventory[i] = "";
+            }
+        }
+
+    }
+}
