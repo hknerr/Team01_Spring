6e79658e777e52749e4bff3b9f819a3f19d8f1cf
s-xtx <48098896+s-xtx@users.noreply.github.com>
2019-05-02 22:50:10 -0400

    removed some puzzles btw

---
+++ Assets/scripts/rocksmove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocksmove : MonoBehaviour
+{
+
+    private void OnMouseDown()
+    {
+        GameObject.Find("rocks").transform.localPosition = new Vector3(-1.9f, -4f, 0);
+    }
+
+}
